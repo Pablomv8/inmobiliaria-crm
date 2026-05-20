d82deaf99e87fc7102e04d7387e17eeef38dac3f
pablomv8 <pabmarval@alum.us.es>
2026-05-20 19:16:05 +0200

    Crear contactos e inmuebles
    He de poblar la base de datos todavía

--- properties/models.py
+++ properties/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Property(models.Model):
+
+    PROPERTY_TYPE_CHOICES = (
+        ('flat', 'Piso'),
+        ('house', 'Casa'),
+        ('solar', 'Solar'),
+        ('office', 'Oficina'),
+        ('local', 'Local'),
+
+    )
+
+    title = models.CharField(max_length=255)
+
+    address = models.CharField(max_length=255)
+
+    city = models.CharField(max_length=100)
+
+    price = models.DecimalField(
+        max_digits=12,
+        decimal_places=2
+    )
+
+    property_type = models.CharField(
+        max_length=20,
+        choices=PROPERTY_TYPE_CHOICES
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
