058767e7a1108fa74db77cae36714a78cd866b08
pablomv8 <pabmarval@alum.us.es>
2026-05-20 20:15:38 +0200

    crear lista de contactos
    Falta  crear, eliminar y editar

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
+from .models import Contact
+
 # Create your views here.
+def contact_list(request):
+
+    contacts= Contact.objects.prefetch_related('properties')
+
+    return render(request, 'contacts/list.html', {
+        'contacts': contacts
+    })
+
+    
